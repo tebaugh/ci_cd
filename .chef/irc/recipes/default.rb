5ebffef2477546bc5bac234bff0cebe90a01a000
Terry Ebaugh <tebaugh@gmail.com>
2014-04-21 15:39:01 -0400

    Modify irc recipe
    Modified the IRC recipe to add a user
    Install the irssi package
    Setup a directory resource
    setup a cookbook_file resource

--- .chef/irc/recipes/default.rb
+++ .chef/irc/recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+User 'tdi' do
+  action :create
+  Comment "Test Driven Infrastructure"
+  home "/home/tdi"
+  manage_home true
+end
+
+package 'irssi' do
+  action :install
+end
+
+directory '/home/tdi/.irssi' do 
+  owner 'tdi'
+  group 'tdi'
+end
+
+cookbook_file '/home/tdi/.irssi/config' do
+  source 'irssi-config'
+  owner 'tdi'
+  group 'tdi'
+end
